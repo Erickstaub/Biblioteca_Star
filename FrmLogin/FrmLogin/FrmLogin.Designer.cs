2cd04bee37734dc04e9b278fff317ec3aed93e59
LEONARDODASILVASCHNE <LEONARDODASILVASCHNE@SCS217777>
2025-08-11 21:31:27 -0300

    desing do login principal quase pronto

--- FrmLogin/FrmLogin/FrmLogin.Designer.cs
+++ FrmLogin/FrmLogin/FrmLogin.Designer.cs
@@ -38,50 +38,59 @@
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
             guna2GroupBox1 = new Guna.UI2.WinForms.Guna2GroupBox();
             guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
             guna2TextBox2 = new Guna.UI2.WinForms.Guna2TextBox();
             guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
             guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            pictureBox1 = new PictureBox();
             guna2GroupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // guna2GroupBox1
             // 
-            guna2GroupBox1.BorderRadius = 30;
+            guna2GroupBox1.BackColor = Color.FromArgb(128, 128, 255);
+            guna2GroupBox1.BorderColor = Color.White;
+            guna2GroupBox1.BorderRadius = 50;
+            guna2GroupBox1.BorderThickness = 0;
             guna2GroupBox1.Controls.Add(guna2Button2);
             guna2GroupBox1.Controls.Add(guna2TextBox2);
             guna2GroupBox1.Controls.Add(guna2TextBox1);
             guna2GroupBox1.Controls.Add(guna2Button1);
-            guna2GroupBox1.CustomBorderColor = Color.Black;
+            guna2GroupBox1.CustomBorderColor = Color.Transparent;
             guna2GroupBox1.CustomizableEdges = customizableEdges9;
             guna2GroupBox1.Font = new Font("Segoe UI", 9F);
             guna2GroupBox1.ForeColor = Color.White;
-            guna2GroupBox1.Location = new Point(60, 251);
+            guna2GroupBox1.Location = new Point(88, 165);
             guna2GroupBox1.Name = "guna2GroupBox1";
             guna2GroupBox1.ShadowDecoration.CustomizableEdges = customizableEdges10;
-            guna2GroupBox1.Size = new Size(655, 377);
+            guna2GroupBox1.Size = new Size(577, 389);
             guna2GroupBox1.TabIndex = 0;
             // 
             // guna2Button2
             // 
+            guna2Button2.BorderRadius = 1;
+            guna2Button2.BorderThickness = 1;
             guna2Button2.CustomizableEdges = customizableEdges1;
             guna2Button2.DisabledState.BorderColor = Color.DarkGray;
             guna2Button2.DisabledState.CustomBorderColor = Color.DarkGray;
             guna2Button2.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
             guna2Button2.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            guna2Button2.FillColor = Color.Silver;
+            guna2Button2.FillColor = Color.White;
             guna2Button2.Font = new Font("Segoe UI", 9F);
-            guna2Button2.ForeColor = Color.White;
-            guna2Button2.Location = new Point(177, 228);
+            guna2Button2.ForeColor = Color.Black;
+            guna2Button2.Location = new Point(185, 248);
             guna2Button2.Name = "guna2Button2";
             guna2Button2.ShadowDecoration.CustomizableEdges = customizableEdges2;
-            guna2Button2.Size = new Size(270, 36);
+            guna2Button2.Size = new Size(231, 36);
             guna2Button2.TabIndex = 3;
             guna2Button2.Text = "Criar Conta";
             // 
             // guna2TextBox2
             // 
+            guna2TextBox2.BackColor = Color.Transparent;
             guna2TextBox2.BorderColor = Color.Gray;
             guna2TextBox2.BorderRadius = 15;
             guna2TextBox2.CustomizableEdges = customizableEdges3;
@@ -90,21 +99,23 @@
             guna2TextBox2.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
             guna2TextBox2.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
             guna2TextBox2.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox2.FillColor = Color.FromArgb(224, 224, 224);
             guna2TextBox2.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
             guna2TextBox2.Font = new Font("Century Gothic", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
             guna2TextBox2.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
-            guna2TextBox2.Location = new Point(42, 51);
+            guna2TextBox2.Location = new Point(42, 121);
             guna2TextBox2.Margin = new Padding(10, 11, 10, 11);
             guna2TextBox2.Name = "guna2TextBox2";
-            guna2TextBox2.PlaceholderForeColor = Color.Gray;
+            guna2TextBox2.PlaceholderForeColor = Color.Black;
             guna2TextBox2.PlaceholderText = "Nome";
             guna2TextBox2.SelectedText = "";
             guna2TextBox2.ShadowDecoration.CustomizableEdges = customizableEdges4;
-            guna2TextBox2.Size = new Size(545, 71);
+            guna2TextBox2.Size = new Size(507, 50);
             guna2TextBox2.TabIndex = 2;
             // 
             // guna2TextBox1
             // 
+            guna2TextBox1.BackColor = Color.White;
             guna2TextBox1.BorderColor = Color.Gray;
             guna2TextBox1.BorderRadius = 15;
             guna2TextBox1.CustomizableEdges = customizableEdges5;
@@ -113,38 +124,51 @@
             guna2TextBox1.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
             guna2TextBox1.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
             guna2TextBox1.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox1.FillColor = Color.FromArgb(224, 224, 224);
             guna2TextBox1.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
             guna2TextBox1.Font = new Font("Century Gothic", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            guna2TextBox1.ForeColor = Color.White;
             guna2TextBox1.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
-            guna2TextBox1.Location = new Point(42, 144);
+            guna2TextBox1.Location = new Point(42, 184);
             guna2TextBox1.Margin = new Padding(10, 11, 10, 11);
             guna2TextBox1.Name = "guna2TextBox1";
-            guna2TextBox1.PlaceholderForeColor = Color.Gray;
+            guna2TextBox1.PlaceholderForeColor = Color.Black;
             guna2TextBox1.PlaceholderText = "Senha";
             guna2TextBox1.SelectedText = "";
             guna2TextBox1.ShadowDecoration.CustomizableEdges = customizableEdges6;
-            guna2TextBox1.Size = new Size(545, 70);
+            guna2TextBox1.Size = new Size(507, 50);
             guna2TextBox1.TabIndex = 1;
             // 
             // guna2Button1
             // 
-            guna2Button1.BorderColor = Color.LimeGreen;
+            guna2Button1.BackColor = Color.Transparent;
+            guna2Button1.BorderColor = Color.White;
             guna2Button1.BorderRadius = 15;
             guna2Button1.CustomizableEdges = customizableEdges7;
             guna2Button1.DisabledState.BorderColor = Color.DarkGray;
             guna2Button1.DisabledState.CustomBorderColor = Color.DarkGray;
             guna2Button1.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
             guna2Button1.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            guna2Button1.FillColor = Color.LimeGreen;
+            guna2Button1.FillColor = Color.SteelBlue;
             guna2Button1.Font = new Font("Century Gothic", 20F);
             guna2Button1.ForeColor = Color.White;
-            guna2Button1.Location = new Point(42, 276);
+            guna2Button1.Location = new Point(74, 290);
             guna2Button1.Name = "guna2Button1";
             guna2Button1.ShadowDecoration.CustomizableEdges = customizableEdges8;
-            guna2Button1.Size = new Size(545, 80);
+            guna2Button1.Size = new Size(448, 50);
             guna2Button1.TabIndex = 0;
-            guna2Button1.Text = "Entrar";
+            guna2Button1.Text = "Conecte-se";
             guna2Button1.Click += guna2Button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(-4, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(790, 676);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
             // 
             // FrmLogin
             // 
@@ -152,10 +176,12 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(785, 677);
             Controls.Add(guna2GroupBox1);
+            Controls.Add(pictureBox1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "FrmLogin";
             Text = "Form1";
             guna2GroupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
@@ -166,5 +192,6 @@
         private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
         private Guna.UI2.WinForms.Guna2Button guna2Button1;
         private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private PictureBox pictureBox1;
     }
 }
--- FrmLogin/FrmLogin/FrmPrincipal.Designer.cs
+++ FrmLogin/FrmLogin/FrmPrincipal.Designer.cs
@@ -30,6 +30,7 @@
         {
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
@@ -43,6 +44,7 @@
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            pictureBox1 = new PictureBox();
             BtnSair = new Guna.UI2.WinForms.Guna2Button();
             BtnAluno = new Guna.UI2.WinForms.Guna2Button();
             BtnEmpre = new Guna.UI2.WinForms.Guna2Button();
@@ -50,11 +52,13 @@
             BtnHome = new Guna.UI2.WinForms.Guna2Button();
             BtnFechar = new Guna.UI2.WinForms.Guna2Button();
             guna2Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // guna2Panel1
             // 
-            guna2Panel1.BackColor = Color.FromArgb(224, 224, 224);
+            guna2Panel1.BackColor = Color.White;
+            guna2Panel1.Controls.Add(pictureBox1);
             guna2Panel1.Controls.Add(BtnSair);
             guna2Panel1.Controls.Add(BtnAluno);
             guna2Panel1.Controls.Add(BtnEmpre);
@@ -66,6 +70,16 @@
             guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges12;
             guna2Panel1.Size = new Size(300, 774);
             guna2Panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(300, 198);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
             // 
             // BtnSair
             // 
@@ -94,10 +108,10 @@
             BtnAluno.DisabledState.CustomBorderColor = Color.DarkGray;
             BtnAluno.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
             BtnAluno.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            BtnAluno.FillColor = Color.FromArgb(224, 224, 224);
+            BtnAluno.FillColor = Color.White;
             BtnAluno.Font = new Font("Century Gothic", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
             BtnAluno.ForeColor = Color.Black;
-            BtnAluno.Location = new Point(-3, 416);
+            BtnAluno.Location = new Point(0, 385);
             BtnAluno.Name = "BtnAluno";
             BtnAluno.ShadowDecoration.CustomizableEdges = customizableEdges4;
             BtnAluno.Size = new Size(300, 68);
@@ -112,10 +126,10 @@
             BtnEmpre.DisabledState.CustomBorderColor = Color.DarkGray;
             BtnEmpre.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
             BtnEmpre.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            BtnEmpre.FillColor = Color.FromArgb(224, 224, 224);
+            BtnEmpre.FillColor = Color.White;
             BtnEmpre.Font = new Font("Century Gothic", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
             BtnEmpre.ForeColor = Color.Black;
-            BtnEmpre.Location = new Point(-3, 342);
+            BtnEmpre.Location = new Point(0, 321);
             BtnEmpre.Name = "BtnEmpre";
             BtnEmpre.ShadowDecoration.CustomizableEdges = customizableEdges6;
             BtnEmpre.Size = new Size(300, 68);
@@ -130,10 +144,10 @@
             BtnAdd.DisabledState.CustomBorderColor = Color.DarkGray;
             BtnAdd.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
             BtnAdd.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            BtnAdd.FillColor = Color.FromArgb(224, 224, 224);
+            BtnAdd.FillColor = Color.White;
             BtnAdd.Font = new Font("Century Gothic", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
             BtnAdd.ForeColor = Color.Black;
-            BtnAdd.Location = new Point(-3, 268);
+            BtnAdd.Location = new Point(0, 259);
             BtnAdd.Name = "BtnAdd";
             BtnAdd.ShadowDecoration.CustomizableEdges = customizableEdges8;
             BtnAdd.Size = new Size(300, 68);
@@ -149,10 +163,10 @@
             BtnHome.DisabledState.CustomBorderColor = Color.DarkGray;
             BtnHome.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
             BtnHome.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            BtnHome.FillColor = Color.FromArgb(224, 224, 224);
+            BtnHome.FillColor = Color.White;
             BtnHome.Font = new Font("Century Gothic", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
             BtnHome.ForeColor = Color.Black;
-            BtnHome.Location = new Point(-3, 194);
+            BtnHome.Location = new Point(0, 195);
             BtnHome.Name = "BtnHome";
             BtnHome.ShadowDecoration.CustomizableEdges = customizableEdges10;
             BtnHome.Size = new Size(300, 68);
@@ -188,6 +202,7 @@
             Name = "FrmPrincipal";
             Text = "FrmPrincipal";
             guna2Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
@@ -200,5 +215,6 @@
         private Guna.UI2.WinForms.Guna2Button BtnAluno;
         private Guna.UI2.WinForms.Guna2Button BtnEmpre;
         private Guna.UI2.WinForms.Guna2Button BtnAdd;
+        private PictureBox pictureBox1;
     }
 }
